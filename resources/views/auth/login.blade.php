6c41e77b3bb8c9eb3c9147b95faf183c66d081c8
thura89 <mozet89@gmail.com>
2023-01-02 12:49:38 +0630

    add favicon in all page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,26 +10,16 @@
     <!-- Prevent the demo from appearing in search engines -->
     <meta name="robots" content="noindex">
 
-    <!--=================================
-Favicon
-=================================-->
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('starfm_favicon/apple-icon-57x57.png') }}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('starfm_favicon/apple-icon-60x60.png') }}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('starfm_favicon/apple-icon-72x72.png') }}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('starfm_favicon/apple-icon-76x76.png') }}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('starfm_favicon/apple-icon-114x114.png') }}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('starfm_favicon/apple-icon-120x120.png') }}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('starfm_favicon/apple-icon-144x144.png') }}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('starfm_favicon/apple-icon-152x152.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('starfm_favicon/apple-icon-180x180.png') }}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('starfm_favicon/android-icon-192x192.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('starfm_favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('starfm_favicon/favicon-96x96.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('starfm_favicon/favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('starfm_favicon/manifest.json') }}">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="{{ asset('starfm_favicon/ms-icon-144x144.png') }}">
-    <meta name="theme-color" content="#ffffff">
+   <!--=================================
+        Favicon
+        =================================-->
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+        <link rel="mask-icon" href="{{ asset('favicon/safari-pinned-tab.svg" color="#5bbad5') }}">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
     <!-- App CSS -->
     <link rel="stylesheet" href="{{ asset('flowdesh_theme/css/style.css') }}" rel="stylesheet">
     <link type="text/css" href="{{ asset('flowdesh_theme/css/app.css') }}" rel="stylesheet">
